d7088266df7f2f2b34c4a7cbbec45180b8f5bb1a
Riyan Dhiman <Riyandhiman14@gmail.com>
2020-05-18 23:59:43 +0530

    commented /dist

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const MONGO_URI = process.env.MONGOLAB_URI||'mongodb://localhost:5000'
 var cri; //Current Room Id
 
 
-app.use(express.static(__dirname, '/dist', {index: false}));
+//app.use(express.static(__dirname, '/dist', {index: false}));
 app.use(session({secret:'ChatApp', resave:true, saveUninitialized: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
