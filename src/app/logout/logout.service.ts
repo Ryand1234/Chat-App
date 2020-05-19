7dba8b7fa93f32bf1183dacc2be084e0b0627e48
Riyan Dhiman <riyandhiman14@gmail.com>
2020-05-19 18:50:53 +0530

    Change method type from GET to POST in logout

--- src/app/logout/logout.service.ts
+++ src/app/logout/logout.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +10,8 @@ export class LogoutService {
 
 	constructor(private http : HttpClient) { }
 
+        httpOptions = new HttpHeaders({'Content-Type':'application/json; 
 	logout() {
-		return this.http.get(this.url);
+		return this.http.get(this.url, { headers: this.httpOptions, responseType: 'json'});
 	}
 }
